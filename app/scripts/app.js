214ae3122fd19b41fcac2ceedc8f034f8c5fbce8
oliver pirkfellner <oliver.pirkfellner.dev@gmail.com>
2016-09-06 20:07:34 +0200

    checkpoint 4 complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,23 +3,24 @@
     $locationProvider
       .html5Mode({
         enabled: true,
-        requreBase: false
-      
+        requireBase: false
     });
   
     $stateProvider
       .state('landing', {
         url: '/',
-        templateUrl: 'templates/landing.html'
+        templateUrl: '/templates/landing.html'
      })
       .state('album', {
         url: '/album',
         templateUrl: '/templates/album.html'
+
      })
       .state('collection', {
-        url: '/collection',
-        templateUrl: '/templates/collection.html'
+      url: '/collection',
+      templateUrl: '/templates/collection.html'
     });
+
 
 }
 
