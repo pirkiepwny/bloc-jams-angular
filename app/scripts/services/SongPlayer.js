3f5db4cbca934417dba740a3b63dccc017f5a148
oliver pirkfellner <oliver.pirkfellner.dev@gmail.com>
2016-09-20 19:08:10 +0200

    Checkpoint and assignment 10 done

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,112 +1,236 @@
 (function() {
-  function SongPlayer(Fixtures) {
-    var SongPlayer = {};
+  function SongPlayer($rootScope, Fixtures) {
+     
+     var SongPlayer = {};
+
+    /**
+    * @desc Current album object retrieved from fixtures
+    * @type {Object}
+    */
     var currentAlbum = Fixtures.getAlbum();
-    var song;
+
+    /**
+    * @desc Buzz object audio file (private)
+    * @type {Object}
+    */
     var currentBuzzObject = null;
-/**
- * @function setSong
- * @desc Stops currently playing song and loads new audio file as currentBuzzObject
- * @param {Object} song
-*/
-  var setSong = function(song) {
+
+    /**
+    * @function setSong (private)
+    * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+    * @param {Object} song
+    */
+    var setSong = function(song) {
+
     if (currentBuzzObject) {
-      stopSong(song);
+      currentBuzzObject.stop();
+
+      if (SongPlayer.currentSong)
+      {
+        SongPlayer.currentSong.playing = null;
+      }
+      
     }
-   /**
-  * @desc Buzz object audio file
-  * @type {Object}
-  */  
+    
     currentBuzzObject = new buzz.sound(song.audioUrl, {
       formats: ['mp3'],
       preload: true
     });
 
+    currentBuzzObject.setVolume(50);
+    
+    /**
+    * @desc Buzz object time updateswhen the seek bar is dragged
+    */
+    currentBuzzObject.bind('timeupdate', function() {
+      $rootScope.$apply(function() {
+        SongPlayer.currentTime = currentBuzzObject.getTime();
+      });
+    });
+
+    /**
+    * @desc Buzz object volume updates when the seek bar is dragged
+    */
+    currentBuzzObject.bind('volumechange', function() {
+      $rootScope.$apply(function() {
+        SongPlayer.volume = currentBuzzObject.getVolume();
+      });
+    });
+ 
     SongPlayer.currentSong = song;
 
-  };
+    };
+    
 
-  var getSongIndex = function(song) {
-    return currentAlbum.songs.indexOf(song);
-  };
+    /**
+    * @function playSong (private)
+    * @desc Plays the loaded currentBuzzObject
+    * @param {Object} song
+    */
+    var playSong = function(song) {
+      currentBuzzObject.play();
+      song.playing = true;
+      SongPlayer.currentAlbum = currentAlbum;
+    };
 
-  SongPlayer.currentSong = null;
+    /**
+    * @function stopSong (private)
+    * @desc Stops the loaded currentBuzzObject
+    * @param {Object} song
+    */
+    var stopSong = function(song) {
+      currentBuzzObject.pause();
+      currentBuzzObject.setTime(null);
+      song.playing = false;
+      SongPlayer.currentAlbum = null;
+      SongPlayer.currentSong = null;
+    };
+    
+    /**
+    * @function getSongIndex (private)
+    * @desc Gets array index of a song from the current album
+    * @param {Object} song
+    */
+    var getSongIndex = function(song) {
+      return currentAlbum.songs.indexOf(song);
+    };
 
-  var playSong = function(song) {
-    currentBuzzObject.play();
-    song.playing = true;
-  }
-  /**
-   * @function play
-   * @desc Play current or new song
-   * @param {Object} song
-   */
-  SongPlayer.play = function(song) {
-    console.log(song);
-    song = song || SongPlayer.currentSong;
-    console.log(song);
 
-    if ( SongPlayer.currentSong !== song ) {
-      console.log("song is not the current playing");
-      setSong(song);
-      playSong(song);
+    /**
+    * @desc Current song variable (public)
+    * @type {Object}
+    */
+    SongPlayer.currentSong = null;
 
-  } else if (SongPlayer.currentSong === song) {
-      if (currentBuzzObject.isPaused()) {
-        currentBuzzObject.play();
+    /**
+    * @desc Current album variable (public)
+    * @type {Object}
+    */
+    SongPlayer.currentAlbum = null;
+
+    /**
+    * @desc Current playback time (in seconds) of currently playing song
+    * @type {Number}
+    */
+    SongPlayer.currentTime = null;
+
+    /**
+    * @desc The volume of the song player
+    * @type {Number}
+    */
+    SongPlayer.volume = null;
+    
+    /**
+    * @function SongPlayer.play (public method of the SongPlayer service)
+    * @desc Uses the private setSong and playSong methods to play music one at a time
+    * @param {Object} song
+    */
+    SongPlayer.play = function(song) {
+      song = song || SongPlayer.currentSong;
+      if (SongPlayer.currentSong !== song) { 
+        setSong(song);
+        playSong(song);
       }
-    }
-  };
-   /**
-   * @function pause
-   * @desc Pause current song
-   * @param {Object} song
-   */
-  SongPlayer.pause = function() {
-    song = song || SongPlayer.currentSong;
-    currentBuzzObject.pause();
-    song.playing = false;
-  };
+      else if (SongPlayer.currentSong === song) {
+        if (currentBuzzObject.isPaused()) {
+          currentBuzzObject.play();
+          song.playing = true;
+        }
+      }         
 
-  SongPlayer.previous = function() {
-    var currentSongIndex = getSongIndex(SongPlayer.currentSong);
-    currentSongIndex--;
+    };
 
-    if (currentSongIndex < 0) {
-      stopSong(song);
+     /**
+     * @function setCurrentTime
+     * @desc Set current time (in seconds) of currently playing song
+     * @param {Number} time
+     */
+    SongPlayer.setCurrentTime = function(time) {
+      if (currentBuzzObject) {
+        currentBuzzObject.setTime(time);
+      }
+    };
 
-    } else {
-      var song = currentAlbum.songs[currentSongIndex];
-      setSong(song);
-      playSong(song);
-    }
-  };
-  SongPlayer.next = function() {
-    var currentSongIndex = getSongIndex(SongPlayer.currentSong);
-    currentSongIndex++;
+     /**
+     * @function setVolume
+     * @desc Set volume of the song player
+     * @param {Number} volume
+     */
+    SongPlayer.setVolume = function(volume) {
+      if (currentBuzzObject) {
+        currentBuzzObject.setVolume(volume)
+      }
+    };
 
-    if (currentSongIndex < 0) {
-      stopSong(song);
-      console.log(currentAlbum.songs[currentSongIndex]);
-    } else {
-      var song = currentAlbum.songs[currentSongIndex];
-      setSong(song);
-      playSong(song);
-      console.log(currentAlbum.songs[currentSongIndex]);
-    }
-  };
+    /**
+    * @function SongPlayer.pause (public method of the SongPlayer service)
+    * @desc Uses the private setSong and playSong methods to pause currently playing music
+    * @param {Object} song
+    */
+    SongPlayer.pause = function(song) {
+      song = song || SongPlayer.currentSong;
+      currentBuzzObject.pause(); // Pauses song, doesn't stop it
+      song.playing = false;
+    }; 
+    
+    /**
+    * @function SongPlayer.playOrPause (public method of the SongPlayer service)
+    * @desc Will either play or pause depending on whether the song is playing (helps with buttons)
+    * @param {Object} song
+    */
+    SongPlayer.playOrPause = function(song) {
+       
+       if (song.playing){
+         SongPlayer.pause(song);
+       }
+       else{
+         SongPlayer.play(song);
+       }
+    };
 
-  var stopSong = function() {
-    currentBuzzObject.stop();
-    SongPlayer.currentSong.playing = null;
+    /**
+    * @function SongPlayer.previous (public method of the SongPlayer service)
+    * @desc Get array index of the song preceding the currentSong
+    */
+    SongPlayer.previous = function() {
+      var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+      currentSongIndex--;
+
+      if (currentSongIndex < 0) {
+        stopSong(SongPlayer.currentSong);
+      } 
+      else {
+        var song = currentAlbum.songs[currentSongIndex];
+        setSong(song);
+        playSong(song);
+      }
+
+    };
+
+    /**
+    * @function SongPlayer.next (public method of the SongPlayer service)
+    * @desc Get array index of the song following the currentSong
+    */
+    SongPlayer.next = function() {
+      var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+      currentSongIndex++;
+
+      if (currentSongIndex >= currentAlbum.songs.length) {
+        stopSong(SongPlayer.currentSong);
+      } 
+      else {
+        var song = currentAlbum.songs[currentSongIndex];
+        setSong(song);
+        playSong(song);
+      }
+
+    };
+
+
+    return SongPlayer;
   }
-
-
-  return SongPlayer;
-}
-
-angular
-  .module('blocJams')
-  .factory('SongPlayer', SongPlayer);
-
+ 
+    angular
+      .module('blocJams')
+      .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -1,5 +1,11 @@
 (function() {
   function seekBar($document) {
+    
+    /**
+    * @function calculatePercent
+    * @desc Calculates percentage of seek bar length
+    * @params {Object} seekBar, {Object} event
+    */
     var calculatePercent = function(seekBar, event) {
       var offsetX = event.pageX - seekBar.offset().left;
       var seekBarWidth = seekBar.width();
@@ -8,60 +14,97 @@
       offsetXPercent = Math.min(1, offsetXPercent);
       return offsetXPercent;
     };
-    
+
     return {
       templateUrl: '/templates/directives/seek_bar.html',
       replace: true,
       restrict: 'E',
-      scope: { },
+      scope: { onChange: '&' },
+      //updates DOM
       link: function(scope, element, attributes) {
-        scope.value = 0;
-        scope.max = 100;
-        
-        var seekBar = $(element);
-        
-        var percentString = function() {
-          var value = scope.value;
-          var max = scope.max;
-          var percent = value / max * 100;
-          return percent + "%";
-        };
-        
-        scope.fillStyle = function() {
-          return {width: percentString()};
-        };
-        
-        scope.onClickSeekBar = function(event) {
-          var percent = calculatePercent(seekBar, event);
-          scope.value = percent * scope.max;
-        };
-        
-        scope.trackThumb = function() {
-          $document.bind('mousemove.thumb', function(event) {
-            var percent = calculatePercent(seekBar, event);
-            scope.$apply(function() {
-              scope.value = percent * scope.max;
-            });
-          
-          });
-          
-        scope.thumbStyle = function() {
-          return {left: percentString()};
-        };
-        
-          
-          $document.bind('mouseup.thumb', function() {
-            $document.unbind('mousemove.thumb');
-            $document.unbind('mouseup.thumb');
-        });
+      // bulk of directive logic is placed here and is returned
+      scope.value = 0;
+      scope.max = 100;
+
+      var seekBar = $(element);
+
+      attributes.$observe('value', function(newValue) {
+        scope.value = newValue;
+      });
+ 
+      attributes.$observe('max', function(newValue) {
+        scope.max = newValue;
+      });
+      
+      /**
+      * @function percentString
+      * @desc Converts percentage number to string type
+      */
+      var percentString = function () {
+        var value = scope.value;
+        var max = scope.max;
+        var percent = value / max * 100;
+        return percent + "%";
       };
-    }
-  }
-  }
+      
+      /**
+      * @function fillStyle
+      * @desc Returns width of bar based on percent
+      */
+      scope.fillStyle = function() {
+        return {width: percentString()};
+      };
+      
+      /**
+      * @function thumbStyle
+      * @desc Updates position of thumb
+      */
+      scope.thumbStyle = function() {
+        return {left: percentString()};
+      };
 
+      /**
+      * @function onClickSeekBar
+      * @desc Updates seek bar value based on bar width and location of click
+      */
+      scope.onClickSeekBar = function(event) {
+        var percent = calculatePercent(seekBar, event);
+        scope.value = percent * scope.max;
+        notifyOnChange(scope.value);
+      };
+      
+      /**
+      * @function trackThumb
+      * @desc Continuously applies change to seek bar value when thumb is dragged
+      */
+      scope.trackThumb = function() {
+        $document.bind('mousemove.thumb', function(event) {
+          var percent = calculatePercent(seekBar, event);
+          scope.$apply(function() {
+            scope.value = percent * scope.max;
+            notifyOnChange(scope.value);
+          });
+        });
+ 
+        $document.bind('mouseup.thumb', function() {
+          $document.unbind('mousemove.thumb');
+          $document.unbind('mouseup.thumb');
+        });
 
-  
+      }; // end of trackThumb method
+
+      var notifyOnChange = function(newValue) {
+        if (typeof scope.onChange === 'function') {
+          scope.onChange({value: newValue});
+        }
+      };
+
+      } // end of link method
+    };
+
+  }
+ 
   angular
     .module('blocJams')
     .directive('seekBar', ['$document', seekBar]);
-}());
+})();
